58c847f5f8cf46b53cc77719434f282cb549cae4
Prajakta <prajakta11.patil@gmail.com>
2025-09-08 13:35:41 +0530

    jdbc crud

--- src/jdbc/crud/operation/Products.java
+++ src/jdbc/crud/operation/Products.java
@@ -33,28 +33,44 @@ public class Products {
 //            }
 
               //insert - using preparedStatement
-              String insertQuery = "insert into products(pid,pname,price) values(?,?,?)";
-              PreparedStatement pstmt =con.prepareStatement(insertQuery);
+//              String insertQuery = "insert into products(pid,pname,price) values(?,?,?)";
+//              PreparedStatement pstmt =con.prepareStatement(insertQuery);
+//
+//              System.out.println("Enter product id: ");
+//              int id = sc.nextInt();
+//              pstmt.setInt(1,id);
+//              sc.nextLine();
+//
+//             System.out.println("Enter product name: ");
+//             String name = sc.nextLine();
+//             pstmt.setString(2,name);
+//
+//            System.out.println("Enter product price: ");
+//            int price = sc.nextInt();
+//            pstmt.setInt(3,price);
+//
+//            int result =pstmt.executeUpdate();
+//            if(result > 0){
+//                System.out.println("Data inserted successfully");
+//            }else {
+//                System.out.println("Data is not inserted.");
+//            }
 
-              System.out.println("Enter product id: ");
-              int id = sc.nextInt();
-              pstmt.setInt(1,id);
-              sc.nextLine();
+            //get All
+            String getAllQuery = "select * from products";
+            PreparedStatement pstmt=con.prepareStatement(getAllQuery);
+            ResultSet rs=pstmt.executeQuery();
 
-             System.out.println("Enter product name: ");
-             String name = sc.nextLine();
-             pstmt.setString(2,name);
-
-            System.out.println("Enter product price: ");
-            int price = sc.nextInt();
-            pstmt.setInt(3,price);
-
-            int result =pstmt.executeUpdate();
-            if(result > 0){
-                System.out.println("Data inserted successfully");
-            }else {
-                System.out.println("Data is not inserted.");
+            System.out.println("Products data: ");
+            while (rs.next()){
+                System.out.println("Product id: "+rs.getInt(1));
+                System.out.println("Product name: "+rs.getString(2));
+                System.out.println("Product price: "+rs.getInt(3));
+                System.out.println("--------------------------------------------");
             }
+
+            con.close();
+            sc.close();
 
         }catch (Exception e){
             System.out.println(e.getMessage());
